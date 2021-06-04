23e90bc4949fd9dc4f9bf3e886688c99111b8c04
oattao123 <72804848+oattao123@users.noreply.github.com>
2021-06-04 17:50:53 +0700

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,15 @@
+﻿// dotnet run in run code in c# 
+// put in terminal 
+using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+}
